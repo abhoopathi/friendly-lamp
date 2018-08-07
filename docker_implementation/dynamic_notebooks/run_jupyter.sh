85fa192441e433ce5df450bde191e4761aa7daed
abhoopathi <abhoopathi@extremenetworks.com>
2018-08-07 05:45:39 -0400

    docker_implementation

---
+++ docker_implementation/dynamic_notebooks/run_jupyter.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+jupyter notebook --certfile=mycert.pem --keyfile mykey.key
---
+++ docker_implementation/project_final_scripts/script.sh
@@ -0,0 +1,3 @@
+
+cd /source_code
+/opt/conda/bin/python run_all_script.py >> /var/log/cron.log 2>&1
